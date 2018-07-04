bd40f829d8c0c80af26583bd8192e08a3e9c360c
andre <andrepestana@gmail.com>
2018-07-03 20:38:42 -0700

    testing...

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function() {
-    var ipb = angular.module('ipb', ['ui.bootstrap', 'socialLogin']);
+    var ipb = angular.module('ipb', ['ui.bootstrap', 'socialLogin', 'ngSanitize']);
     ipb.controller('MainController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
         $scope.message = "Loading...";
         $scope.articlesURL = 'https://script.google.com/macros/s/AKfycbwI04gbnD_WGUgl3oMC_nR1omjahZqmkL2gbaUp-ku7rCqoBDI/exec?action=getArticles';        
@@ -14,5 +14,10 @@
           }, function errorCallback(response) {
             $scope.message = 'Failed';
           });
-    }]);        
+    }]);
+    
+    ipb.filter('markdown', function() {
+    var converter = new Showdown.converter();
+    return converter.makeHtml;
+});
 })();
